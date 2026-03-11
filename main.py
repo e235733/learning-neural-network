f0d7d9d59d925723d53fcea816094edf9acf506d
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-03-11 19:58:40 +0900

    fix: main-logic

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ from xor_dataset import XorDataset
 from plotter import Plotter
 from neural_network import NeuralNetworkModel
 
+import numpy as np
+
 def main():
 
-    NUM_STEPS = 100
+    NUM_STEPS = 2000
+    ETA = 2
 
-    data = XorDataset(100)
+    data = XorDataset(250)
 
-    model = NeuralNetworkModel()
+    model = NeuralNetworkModel(data.X, data.Y, ETA)
 
     plotter = Plotter(0.1, data.X, data.Y)
 
     for step in range(NUM_STEPS):
         model.shift()
-        plotter.show(model)
+        model.calc_loss()
+        if step % 20 == 0:
+            plotter.show(model)
+
+    # 正解率の計算
+    Y_pred = np.argmax(model.P, axis=1)
+    accuracy = np.mean(Y_pred == data.Y)
+    print(f"Accuracy: {accuracy * 100:.2f}%")
+
+    plotter.finish()
 
 if __name__ == "__main__":
     main()
