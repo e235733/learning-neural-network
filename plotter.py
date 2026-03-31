b387d2ac66df5dbcb72e740bfe387c4a55f9fb01
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-03-31 18:21:56 +0900

    fix: show 2D only when input dimention is 2D

--- plotter.py
+++ plotter.py
@@ -7,25 +7,40 @@ class Plotter:
         self.X = X
         self.Y = np.argmax(Y, axis=1) if Y.ndim > 1 else Y
         self.is_detail_mode = is_detail_mode
+        self.input_dim = X.shape[1]
         
         if self.is_detail_mode:
             self.fig, self.axs = plt.subplots(2, 3, figsize=(12, 7))
             plt.tight_layout()
             plt.subplots_adjust(hspace=0.4, wspace=0.3, top=0.9)
-        else: # 'detail'
-            self.fig = plt.figure(figsize=(12, 6))
-            self.ax_loss = self.fig.add_subplot(1, 2, 1)
-            self.ax_data = self.fig.add_subplot(1, 2, 2)
+            self.ax_loss = self.axs[0, 0]
+            self.ax_data = self.axs[0, 1]
+            if self.input_dim != 2:
+                self.ax_data.set_axis_off()
+                self.ax_data.set_title(f"No 2D plot ({self.input_dim}D)")
+        else:
+            if self.input_dim == 2:
+                self.fig = plt.figure(figsize=(12, 6))
+                self.ax_loss = self.fig.add_subplot(1, 2, 1)
+                self.ax_data = self.fig.add_subplot(1, 2, 2)
+            else:
+                self.fig = plt.figure(figsize=(6, 6))
+                self.ax_loss = self.fig.add_subplot(1, 1, 1)
+                self.ax_data = None
 
     def show(self, model):
+        self._show_loss(model)
+        
+        if self.input_dim == 2:
+            self.ax_data.cla()
+            self._plot_2d(model, self.ax_data)
+            
         if self.is_detail_mode:
-            self._show_detail(model)
-        else:
-            self._show_default(model)
+            self._show_network_stats(model)
         
         plt.pause(self.interval)
 
-    def _show_default(self, model):
+    def _show_loss(self, model):
         self.ax_loss.cla()
         self.ax_loss.plot(model.loss_history, color='purple', linewidth=2)
         self.ax_loss.set_title("Learning Curve")
@@ -33,21 +48,7 @@ class Plotter:
         self.ax_loss.set_ylabel("Loss")
         self.ax_loss.grid(True)
 
-        self.ax_data.cla()
-        self._plot_2d(model, self.ax_data)
-
-    def _show_detail(self, model):
-        ax_loss = self.axs[0, 0]
-        ax_loss.cla()
-        ax_loss.plot(model.loss_history, color='purple', linewidth=2)
-        ax_loss.set_title("Learning Curve")
-        ax_loss.set_xlabel("Iteration")
-        ax_loss.set_ylabel("Loss")
-
-        ax_data = self.axs[0, 1]
-        ax_data.cla()
-        self._plot_2d(model, ax_data)
-
+    def _show_network_stats(self, model):
         ax_w = self.axs[1, 0]
         ax_w.cla()
         for i, w in enumerate(model.W):
