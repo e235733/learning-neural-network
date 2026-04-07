2de5aa45bc231245988772a08a96a5292a61b139
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-04-07 15:46:52 +0900

    feat: split mnist dataset to 60000 train data and 10000 test data

--- mnist_dataset.py
+++ mnist_dataset.py
@@ -5,8 +5,11 @@ class MnistDataset:
     def __init__(self, n_samples=2000):
         mnist = fetch_openml('mnist_784', version=1, as_frame=False)
 
-        X = mnist.data[:n_samples].astype(np.float32)
-        Y = mnist.target[:n_samples].astype(np.int32)
+        X = mnist.data[:n_samples].astype(np.float32) / 255.0
+        Y = np.identity(10)[mnist.target[:n_samples].astype(np.int32)]
 
-        self.X = X / 255.0
-        self.Y = np.identity(10)[Y]
+        self.X_train = X[:60000]
+        self.Y_train = Y[:60000]
+
+        self.X_test = X[60000:]
+        self.Y_test = Y[60000:]
